3e9f78c4d542a91e0faf731241e19221a84ec148
seleznev <mnseleznev@mail.ru>
2019-02-06 19:26:50 +0300

    Start2

--- app/src/main/java/com/example/myweather/WeatherActivity.java
+++ app/src/main/java/com/example/myweather/WeatherActivity.java
@@ -1,21 +1,20 @@
 package com.example.myweather;
 
-import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class WeatherActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_weather);
         Button button = findViewById(R.id.button);
-        EditText editText = findViewById(R.id.editText);
+        button.setOnClickListener(onClickListener);
 
 
     }
@@ -23,7 +22,9 @@ public class MainActivity extends AppCompatActivity {
     View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-          String result = Weather.getWeather();
+            EditText editText = findViewById(R.id.editText);
+          String result = "test";//Weather.getWeather();
+          editText.setText(result);
         }
     };
 
